4f03923e905e27dd1dbd4d4298aa2653353281a8
fares boutriga <boutrigafares@gmail.com>
2023-07-07 12:26:08 +0100

    addService

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AddService from "./components/AddService/AddService.jsx";
+
+function App() {
+  return (
+    <div >
+      <AddService/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/damyy.js
@@ -0,0 +1,14 @@
+const damyy=[
+    {
+        id:1,
+        category:"category",
+        companyName:"company Name",
+        description:"description",
+        costPerDay:500,
+        logo:"'https://res.cloudinary.com/dgu7vsmfd/image/upload/v1544375649/NGA_Screenshot_510x365.jpg",
+        images:"'https://res.cloudinary.com/dgu7vsmfd/image/upload/v1544375649/NGA_Screenshot_510x365.jpg",
+        address:"address",
+        createdAt:"createdAt",
+        updatedAt:"updatedAt"
+    }
+]
