287fd30416a48ef6b9989cb1e0c814524de5c56a
fares boutriga <boutrigafares@gmail.com>
2023-07-07 16:02:15 +0100

    OneService

--- src/components/OneService/OneService.jsx
+++ src/components/OneService/OneService.jsx
@@ -3,7 +3,33 @@ import './OneService.css'
 function OneService() {
   return (
     <div>
-        hellow from one serviec 
+        <ul>
+    <li className="card">
+     
+            <img className="img" src='https://res.cloudinary.com/dgu7vsmfd/image/upload/v1544375649/NGA_Screenshot_510x365.jpg' />
+
+        <div className="card-body">
+           
+                <div href="utilidata-national-governors-association-meeting">
+                    <span className="pre-heading">Blog</span>
+                    <div className="title">
+                        <h3>Test this responsively to see the horizontal magik</h3>
+                    </div>
+                    <p className="meta">
+                        <span className="author">Utilidata</span>
+                        <span> | </span>
+                    </p>
+                </div>
+    
+            <div className="chips">
+                <button className="chip">Government</button>
+                <button className="chip">Test</button>
+                <button className="chip">Utilidata</button>
+            </div>
+        </div>
+    </li>
+        
+    </ul> 
     </div>
   )
 }
